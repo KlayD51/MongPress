f75b0d03b264c66c3655004be58ad1ecfd70b482
Klay-Michael Dehaney <kdehaney53@gmail.com>
2022-09-11 10:27:08 -0400

    middleware function and get one

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -14,12 +14,9 @@ try{
 })
 
 //Get one from Mailing List
-router.get('/:id' ,(req, res) => {
-try{
-res.json(req.params.id)
-} catch (err) {
-    res.status(500).json({message: err.message})
-}
+//using getSubscriber middleware
+router.get('/:id', getSubscriber,(req, res) => {
+res.send(res.subscriber.name)
 })
 
 //Create one for Mailing List
@@ -45,4 +42,20 @@ router.patch('/:id' ,(req, res) => {
 router.post('/:id' ,(req, res) => {
     
 })
+
+//middleware function
+async function getSubscriber(req, res, next){
+    let subcscriber
+try{
+subscriber = await Subscriber.findById(req.params.id)
+if (subscriber == null){
+    return res.status(404).json({message: 'Could not find Sub'})
+}
+} catch (err) {
+return res.status(500).json({message : err.message})
+}
+res.subscriber = subscriber;//allows us to call subscribers that we set here in the other requests
+next()
+}
+
 module.exports = router;
